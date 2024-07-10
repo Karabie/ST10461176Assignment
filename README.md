b3c928260f8fbd2fb14b6f727d1b24f490363ad1
@KaraboMakoro <k.m.makoro@gmail.com>
2024-07-10 19:06:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ST10461176Assignment
+ Programming 1B Assignment 1
---
+++ src/main/java/rc/st10461176assignment/ST10461176Assignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rc.st10461176assignment;
+
+/**
+ *
+ * @author kmmak
+ */
+public class ST10461176Assignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
